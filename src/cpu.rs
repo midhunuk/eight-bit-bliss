921ff1a5f8533dae09c82e0b6097bec5fbf3c870
Midhun Kumar <midhunukumar5@gmail.com>
2024-03-25 01:16:54 +0530

    memory implementaion

--- src/cpu.rs
+++ src/cpu.rs
@@ -4,6 +4,7 @@ pub struct Cpu {
     pub register_x: u8,
     pub status: u8,
     pub program_counter: u16,
+    memory: [u8; 0xFFFF],
 }
 
 impl Cpu {
@@ -13,17 +14,58 @@ impl Cpu {
             register_x: 0,
             status: 0,
             program_counter: 0,
+            memory: [0; 0xFFFF],
         }
     }
 
-    pub fn interpret(&mut self, program: Vec<u8>) {
-        validate_program(&program);
+    fn mem_read(&self, addr: u16) -> u8 {
+        self.memory[addr as usize]
+    }
 
-        self.program_counter = 0;
+    fn mem_write(&mut self, addr: u16, data: u8) {
+        self.memory[addr as usize] = data;
+    }
+
+    fn mem_read_u16(&mut self, pos: u16) -> u16 {
+        let lower_byte = self.mem_read(pos) as u16;
+        let higher_byte = self.mem_read(pos + 1) as u16;
+        (higher_byte << 8) | (lower_byte as u16)
+    }
+ 
+    fn mem_write_u16(&mut self, pos: u16, data: u16) {
+        let higher_byte = (data >> 8) as u8;
+        let lower_byte = (data & 0xff) as u8;
+        self.mem_write(pos, lower_byte);
+        self.mem_write(pos + 1, higher_byte);
+    }
+ 
+
+    pub fn load_and_run(&mut self, program: Vec<u8>) {
+        self.load(program);
+        self.reset();
+        self.run();
+    }
+
+    pub fn load(&mut self, program: Vec<u8>) {
+        validate_program(&program);
+        self.memory[0x8000 .. (0x8000 + program.len())].copy_from_slice(&program[..]);
+        self.mem_write_u16(0xFFFC, 0x8000);
+    }
+
+    pub fn reset(&mut self) {
+        self.register_a = 0;
+        self.register_x = 0;
+        self.status = 0;
+ 
+        self.program_counter = self.mem_read_u16(0xFFFC);
+    }
+ 
+    pub fn run(&mut self) {
+
         loop {
-            let operation_code = self.get_next_byte_and_update_program_counter(&program);
+            let operation_code = self.get_next_byte_and_update_program_counter();
             match operation_code {
-                0xA9 => self.lda(&program),
+                0xA9 => self.lda(),
                 0xAA => self.tax(),
                 0xE8 => self.inx(),
                 0x00 => return,
@@ -32,17 +74,14 @@ impl Cpu {
         }
     }
 
-    fn get_next_byte_and_update_program_counter(&mut self, program: &Vec<u8>) -> u8 {
-        let byte_code = program[self.program_counter as usize];
+    fn get_next_byte_and_update_program_counter(&mut self,) -> u8 {
+        let byte_code = self.mem_read(self.program_counter);
         self.program_counter += 1;
         byte_code
     }
 
-    fn lda(&mut self, program: &Vec<u8>) {
-        let param = self.get_next_byte_and_update_program_counter(program);
-        if self.program_counter as usize == program.len() {
-            panic!("wrong argument for load accumulator")
-        }
+    fn lda(&mut self) {
+        let param = self.get_next_byte_and_update_program_counter();
         self.register_a = param;
         self.update_zero_and_negative_flags(self.register_a);
     }
--- src/cpu/tests.rs
+++ src/cpu/tests.rs
@@ -1,137 +1,128 @@
 use super::*;
 
+const PROGRAM_START_VALUE: u16 = 0x8000;
+
 #[test]
 #[should_panic(expected = "program is empty")]
 fn empty_program() {
     let mut cpu = Cpu::new();
-    cpu.interpret(vec![]);
+    cpu.load_and_run(vec![]);
 }
 
 #[test]
 #[should_panic(expected = "program should end with 0x00")]
 fn program_without_0x00_ending() {
     let mut cpu = Cpu::new();
-    cpu.interpret(vec![0x01, 0x03]);
+    cpu.load_and_run(vec![0x01, 0x03]);
 }
 
 #[test]
 fn program_0x00_program_counter_updates_to_1_and_exits() {
     let mut cpu = Cpu::new();
-    cpu.interpret(vec![0x00]);
-    assert_eq!(cpu.program_counter, 1);
+    cpu.load_and_run(vec![0x00]);
+    assert_eq!(cpu.program_counter, PROGRAM_START_VALUE + 1);
 }
 
 #[test]
 fn lda_param_is_loaded_to_register_a() {
     let mut cpu = Cpu::new();
-    cpu.interpret(vec![0xA9, 0x11, 0x00]);
+    cpu.load_and_run(vec![0xA9, 0x11, 0x00]);
     assert_eq!(cpu.register_a, 0x11);
-    assert_eq!(cpu.program_counter, 3);
-}
-
-#[test]
-#[should_panic(expected = "wrong argument for load accumulator")]
-fn lda_param_argument_wrong() {
-    let mut cpu = Cpu::new();
-    cpu.interpret(vec![0xA9, 0x00]);
+    assert_eq!(cpu.program_counter, PROGRAM_START_VALUE + 3);
 }
 
 #[test]
 fn lda_param_is_0_zero_flag_is_set() {
     let mut cpu = Cpu::new();
-    cpu.interpret(vec![0xA9, 0x00, 0x00]);
+    cpu.load_and_run(vec![0xA9, 0x00, 0x00]);
     assert_eq!(cpu.register_a, 0x00);
-    assert_eq!(cpu.program_counter, 3);
+    assert_eq!(cpu.program_counter, PROGRAM_START_VALUE + 3);
     assert_eq!(cpu.status, 0b0000_0010);
 }
 
-#[test]
-fn lda_param_is_0_zero_flag_is_set_without_affecting_other_flags() {
-    let mut cpu = Cpu::new();
-    cpu.status = 0b0110_1101;
-    cpu.interpret(vec![0xA9, 0x00, 0x00]);
-    assert_eq!(cpu.register_a, 0x00);
-    assert_eq!(cpu.program_counter, 3);
-    assert_eq!(cpu.status, 0b0110_1111);
-}
 
 #[test]
 fn lda_param_is_negative_negative_flag_is_set() {
     let mut cpu = Cpu::new();
-    cpu.interpret(vec![0xA9, 0xA0, 0x00]);
+    cpu.load_and_run(vec![0xA9, 0xA0, 0x00]);
     assert_eq!(cpu.register_a, 0xA0);
-    assert_eq!(cpu.program_counter, 3);
+    assert_eq!(cpu.program_counter, PROGRAM_START_VALUE + 3);
     assert_eq!(cpu.status, 0b1000_0000);
 }
 
-#[test]
-fn lda_param_is_negative_negative_flag_is_set_without_affecting_other_flags() {
-    let mut cpu = Cpu::new();
-    cpu.status = 0b0110_1111;
-    cpu.interpret(vec![0xA9, 0xA0, 0x00]);
-    assert_eq!(cpu.register_a, 0xA0);
-    assert_eq!(cpu.program_counter, 3);
-    assert_eq!(cpu.status, 0b1110_1101);
-}
 
 #[test]
 fn tax_moves_accumulator_to_register_x() {
     let mut cpu = Cpu::new();
-    cpu.interpret(vec![0xA9, 0x11, 0xAA, 0x00]);
+    cpu.load_and_run(vec![0xA9, 0x11, 0xAA, 0x00]);
     assert_eq!(cpu.register_a, 0x11);
     assert_eq!(cpu.register_x, 0x11);
-    assert_eq!(cpu.program_counter, 4);
+    assert_eq!(cpu.program_counter, PROGRAM_START_VALUE + 4);
 }
 
 #[test]
-fn tax_accumalator_value_is_0_zero_flag_is_set_without_affecting_other_flags() {
+fn tax_accumalator_value_is_0_zero_flag_is_set() {
     let mut cpu = Cpu::new();
-    cpu.status = 0b0110_1101;
-    cpu.interpret(vec![0xA9, 0x00, 0xAA, 0x00]);
+    cpu.load_and_run(vec![0xA9, 0x00, 0xAA, 0x00]);
     assert_eq!(cpu.register_a, 0x00);
     assert_eq!(cpu.register_x, 0x00);
-    assert_eq!(cpu.program_counter, 4);
-    assert_eq!(cpu.status, 0b0110_1111);
+    assert_eq!(cpu.program_counter, PROGRAM_START_VALUE + 4);
+    assert_eq!(cpu.status, 0b0000_0010);
 }
 
 #[test]
-fn tax_accumalator_value_is_negative_negative_flag_is_set_without_affecting_other_flags() {
+fn tax_accumalator_value_is_negative_negative_flag_is_set() {
     let mut cpu = Cpu::new();
-    cpu.status = 0b0110_1111;
-    cpu.interpret(vec![0xA9, 0xA0, 0xAA, 0x00]);
+    cpu.load_and_run(vec![0xA9, 0xA0, 0xAA, 0x00]);
     assert_eq!(cpu.register_a, 0xA0);
     assert_eq!(cpu.register_x, 0xA0);
-    assert_eq!(cpu.program_counter, 4);
-    assert_eq!(cpu.status, 0b1110_1101);
+    assert_eq!(cpu.program_counter, PROGRAM_START_VALUE + 4);
+    assert_eq!(cpu.status, 0b1000_0000);
 }
 
 #[test]
-fn inx_value_incremented_by_1(){
+fn inx_value_incremented_by_1() {
     let mut cpu = Cpu::new();
-    cpu.interpret(vec![0xA9, 0x11, 0xAA, 0xE8, 0x00]);
+    cpu.load_and_run(vec![0xA9, 0x11, 0xAA, 0xE8, 0x00]);
     assert_eq!(cpu.register_a, 0x11);
     assert_eq!(cpu.register_x, 0x12);
-    assert_eq!(cpu.program_counter, 5); 
+    assert_eq!(cpu.program_counter, PROGRAM_START_VALUE + 5);
 }
 
 #[test]
-fn inx_value_incremented_by_1_zero_flag_set(){
+fn inx_value_incremented_by_1_zero_flag_set() {
     let mut cpu = Cpu::new();
-    cpu.status = 0b0110_1101;
-    cpu.interpret(vec![0xA9, 0xFF, 0xAA, 0xE8, 0x00]);
+    cpu.load_and_run(vec![0xA9, 0xFF, 0xAA, 0xE8, 0x00]);
     assert_eq!(cpu.register_a, 0xFF);
     assert_eq!(cpu.register_x, 0x00);
-    assert_eq!(cpu.program_counter, 5); 
-    assert_eq!(cpu.status, 0b0110_1111);
+    assert_eq!(cpu.program_counter, PROGRAM_START_VALUE + 5);
+    assert_eq!(cpu.status, 0b0000_0010);
 }
 
 #[test]
-fn inx_value_incremented_by_1_negative_flag_set(){
+fn inx_value_incremented_by_1_negative_flag_set() {
     let mut cpu = Cpu::new();
-    cpu.status = 0b0110_1111;
-    cpu.interpret(vec![0xA9, 0x7F, 0xAA, 0xE8, 0x00]);
+    cpu.load_and_run(vec![0xA9, 0x7F, 0xAA, 0xE8, 0x00]);
     assert_eq!(cpu.register_a, 0x7F);
     assert_eq!(cpu.register_x, 0x80);
-    assert_eq!(cpu.program_counter, 5); 
-    assert_eq!(cpu.status, 0b1110_1101);
+    assert_eq!(cpu.program_counter, PROGRAM_START_VALUE + 5);
+    assert_eq!(cpu.status, 0b1000_0000);
+}
+
+#[test]
+fn inx_value_incremented_over_flow() {
+    let mut cpu = Cpu::new();
+    cpu.load_and_run(vec![0xA9, 0xFF, 0xAA, 0xE8, 0xE8, 0x00]);
+    assert_eq!(cpu.register_a, 0xFF);
+    assert_eq!(cpu.register_x, 0x01);
+    assert_eq!(cpu.program_counter, PROGRAM_START_VALUE + 6);
+    assert_eq!(cpu.status, 0b0000_0000);
+}
+
+#[test]
+fn write_and_read_16_bit_memory(){
+    let mut cpu = Cpu::new();
+    cpu.mem_write_u16(0x2000, 0xFF00);
+    let value = cpu.mem_read_u16(0x2000);
+    assert_eq!(value, 0xFF00);
 }
